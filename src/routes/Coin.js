aad3ddac212262b39c821c5949f6b63504074fb2
Demirov Ayaz <99310621+DemirovAYAZ@users.noreply.github.com>
2023-07-19 21:15:35 +0400

    Update Coin.js

--- src/routes/Coin.js
+++ src/routes/Coin.js
@@ -8,7 +8,7 @@ const Coin = () => {
 
     const params = useParams()
 
-    const [coin, setCoin] = useState({})
+    const [coin, setCoin] = useState({}) 
 
     const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
 
